2b0b4f4be1c00ce69e133b9b8e4e06f24d500894
Ricky Garcia <rg3646@outlook.com>
2023-03-29 14:53:33 -0600

    remove comments

--- algos/twoSum/twoSum.ts
+++ algos/twoSum/twoSum.ts
@@ -1,23 +1,13 @@
 const twoSum = (nums: number[], target: number): number[] => {
-    // Create an empty object to store the complements of the numbers in the array
-    const complements: {[key: number]: number} = Object.create(null);
-  
-    // Loop through the array of numbers
-    for (let [i, num] of nums.entries()) {
-      // Calculate the complement of the current number
-      const complement = target - num;
-      
-      // If the complement is in the complements object, return the indices of the two numbers
-      if (complement in complements) {
-        return [complements[complement], i];
-      }
-  
-      // Otherwise, add the current number to the complements object with its index as the value
-      complements[num] = i;
+  const complements: {[key: number]: number} = Object.create(null);
+  for (let [i, num] of nums.entries()) {
+    const complement = target - num;
+    if (complement in complements) {
+      return [complements[complement], i];
     }
-  
-    // Return an empty array if no solution is found
-    return [];
+    complements[num] = i;
   }
+  return [];
+};
 
-  export default twoSum;
+export default twoSum;
